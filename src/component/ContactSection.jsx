f6901de5954857a1be51543d18174e01ae117d6b
Charith Miriharnga <charithmihiranga453@gmail.com>
2025-07-13 16:08:56 +0530

    project done.

---
+++ src/component/ContactSection.jsx
@@ -0,0 +1,161 @@
+import {Instagram, Linkedin, Mail, MapPin, Phone, Send} from "lucide-react";
+import {cn} from "@/lib/util.js";
+
+export const ContactSection = () => {
+
+    const onSubmit = async () => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "5a1880cf-6c48-4d47-a3b5-49a7387668e0");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            alert(res.message);
+        }
+    };
+
+    return (
+        <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+                    Get In <span className="text-primary">Touch</span>
+                </h2>
+
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    Have a project in mind or want to collaborate? Feel free to reach out.
+                    I'm always open to discussing new opportunities.
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                    <div className="space-y-8">
+                        <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+
+                        <div className="space-y-6 justify-center">
+
+                            <div className="flex items-start space-x-4">
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <Mail className="text-primary" />
+                                </div>
+                                <div>
+                                    <h4 className="font-medium">Email</h4>
+                                    <a
+                                        href="mailto:charithmihiranga453@gmail.com"
+                                        className="text-muted-foreground hover:text-primary transition-colors"
+                                    >
+                                        charithmihiranga453@gmail.com
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="flex items-start space-x-4">
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <Phone className="text-primary" />
+                                </div>
+                                <div>
+                                    <h4 className="font-medium">Phone</h4>
+                                    <a
+                                        href="tel:+94778986962"
+                                        className="text-muted-foreground hover:text-primary transition-colors"
+                                    >
+                                           +(94) 77 8986 962
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="flex items-start space-x-4">
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <MapPin className="text-primary" />
+                                </div>
+                                <div>
+                                    <h4 className="font-medium">Location</h4>
+                                    <a
+                                        className="text-muted-foreground hover:text-primary transition-colors"
+                                    >
+                                        Dambulla Rd, Matale
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="pt-8">
+                            <h4 className="font-medium mb-4"> Connect With Me</h4>
+                            <div className="flex space-x-4 justify-center">
+                                <a href="#" target="_blank">
+                                    <Linkedin/>
+                                </a>
+                                <a href="#" target="_blank">
+                                    <Instagram/>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div
+                        className="bg-card p-8 rounded-lg shadow-xs"
+                        onSubmit={onSubmit}
+                    >
+                        <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+
+                        <form className="space-y-6">
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium mb-2"> Your Name</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    required
+                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                                    placeholder="Your Name..."
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium mb-2"> Your Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    required
+                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                                    placeholder="example@gmail.com"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="message" className="block text-sm font-medium mb-2"> Your Message</label>
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    required
+                                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none"
+                                    placeholder="Your Message..."
+                                />
+                            </div>
+
+                            <button type="submit"  className={cn(
+                                "cosmic-button w-full flex items-center justify-center gap-2",
+
+                            )}>
+                                <Send size={16}/>
+                                Send Message
+                            </button>
+
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import {ThemeToggle} from "@/component/ThemeToggle.jsx";
+import {StarBackground} from "@/component/StarBackground.jsx";
+import {Navar} from "@/component/Navar.jsx";
+import {HeroSection} from "@/component/HeroSection.jsx";
+import {AboutSection} from "@/component/AboutSection.jsx";
+import {SkillsSection} from "@/component/SkillsSection.jsx";
+import {ProjectSection} from "@/component/ProjectSection.jsx";
+import {ContactSection} from "@/component/ContactSection.jsx";
+import {Footer} from "@/component/Footer.jsx";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/*Theme Toggle*/}
+            <ThemeToggle />
+            {/*Background Effects*/}
+            <StarBackground />
+            {/*Navbar*/}
+            <Navar />
+            {/*Main content*/}
+            <main>
+                <HeroSection />
+                <AboutSection />
+                <SkillsSection />
+                <ProjectSection />
+                <ContactSection />
+            </main>
+            {/*Footer*/}
+            <Footer />
+        </div>
+    );
+}
