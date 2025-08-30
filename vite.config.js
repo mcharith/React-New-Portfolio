f7f0daff86fc66170c1d2182977b4c29f076316a
Charith Miriharnga <charithmihiranga453@gmail.com>
2025-08-30 08:41:16 +0530

    Fix Vite base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname,"./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/",
 })
